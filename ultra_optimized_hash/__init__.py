fc4f4251d6d8e74be59ef1f7ec337509a94412fe
vincentiusyoshua <147534658+vincentiusyoshua@users.noreply.github.com>
2024-10-26 10:26:16 +0700

    Delete ultra_optimized_hash directory

--- ultra_optimized_hash/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import UltraOptimizedHash 
-from .utils import SecurityConfig
-
-__version__ = "0.1.0"
-__all__ = ["UltraOptimizedHash", "SecurityConfig"]
--- ultra_optimized_hash/utils.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class SecurityConfig:
-    """Configuration class for different security levels"""
-    rounds: int
-    memory_cost: int
-    transform_iterations: int
-    compression_level: int
